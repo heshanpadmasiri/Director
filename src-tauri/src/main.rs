6baa5c282fe107ec09e1c716743d9c2d62510847
Heshan Padmasiri <hpheshan@gmail.com>
2023-11-26 12:49:57 +0530

    Show image preview

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,25 +1,48 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::path::PathBuf;
+use std::{io::Read, path::PathBuf, sync::Mutex};
 
 use tauri::State;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 struct AppState {
-    path: PathBuf,
+    path: Mutex<PathBuf>,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+enum FileKind {
+    Image,
+    Other,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+struct FilePreview {
+    kind: FileKind,
+    content: String,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+enum PreviewData {
+    File(FilePreview),
+    Directory(String),
 }
 
 fn initial_state() -> AppState {
     AppState {
-        path: get_path(),
+        path: Mutex::new(starting_path()),
     }
 }
 
 fn main() {
     tauri::Builder::default()
         .manage(initial_state())
-        .invoke_handler(tauri::generate_handler![get_files, get_current_path])
+        .invoke_handler(tauri::generate_handler![
+            get_files,
+            get_current_path,
+            go_to_parent,
+            get_preview
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -38,21 +61,72 @@ impl File {
     }
 }
 
-// TODO: take an optional path (current_dir)
-// TODO: return type and file name
 #[tauri::command]
 fn get_files(state: State<AppState>) -> Vec<String> {
-    get_files_in_directory(&state.path).iter().map(|file| file.name()).collect()
+    let current_path = state.path.lock().unwrap();
+    get_files_in_directory(&current_path)
+        .iter()
+        .map(|file| file.name())
+        .collect()
+}
+
+#[tauri::command]
+fn get_preview(index: usize, state: State<AppState>) -> PreviewData {
+    let current_path = state.path.lock().unwrap();
+    let files = get_files_in_directory(&current_path);
+    match &files[index] {
+        File::File(path) => PreviewData::File(file_preview(path)),
+        File::Directory(path) => PreviewData::Directory(path.to_str().unwrap().to_string()),
+    }
+}
+
+fn file_preview(path: &PathBuf) -> FilePreview {
+    match file_kind(path) {
+        FileKind::Image => FilePreview {
+            kind: FileKind::Image,
+            content: image_content(path),
+        },
+        FileKind::Other => FilePreview {
+            kind: FileKind::Other,
+            content: "NA".to_string(),
+        },
+    }
+}
+
+fn image_content(path: &PathBuf) -> String {
+    let mut file = std::fs::File::open(path).unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    base64::encode(buffer)
+}
+
+fn file_kind(path: &PathBuf) -> FileKind {
+    match path.extension() {
+        Some(extension) => match extension.to_str().unwrap() {
+            "jpg" | "png" | "gif" => FileKind::Image,
+            _ => FileKind::Other,
+        },
+        None => FileKind::Other,
+    }
+}
+
+#[tauri::command]
+fn go_to_parent(state: State<AppState>) {
+    let path = state.path.lock().unwrap().parent().unwrap().to_path_buf();
+    *state.path.lock().unwrap() = path;
 }
 
 #[tauri::command]
 fn get_current_path(state: State<AppState>) -> String {
-    state.path.file_name().unwrap().to_str().unwrap().to_string()
-}
-
-#[tauri::command]
-fn get_starting_path() -> String {
-    get_path().file_name().unwrap().to_str().unwrap().to_string()
+    state
+        .path
+        .lock()
+        .unwrap()
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string()
 }
 
 fn get_files_in_directory(path: &PathBuf) -> Vec<File> {
@@ -69,7 +143,7 @@ fn get_files_in_directory(path: &PathBuf) -> Vec<File> {
     files
 }
 
-fn get_path() -> PathBuf {
+fn starting_path() -> PathBuf {
     let path = std::env::current_dir().unwrap();
     let path = path.to_str().unwrap();
     PathBuf::from(path)
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,71 @@
 import { invoke } from "@tauri-apps/api/tauri";
 
-let fileList: HTMLElement | null;
-let currentDir: string | null;
-
-async function getFiles() {
-    console.log("test1");
+async function updateFileList() {
+    const fileList = document.querySelector("#fileList");
     if (fileList) {
+        removeChildren(fileList);
         const files = await invoke("get_files");
-        files.forEach(fileName => {
+        files.forEach((fileName, index) => {
             const listItem = document.createElement("li");
+            listItem.onclick = () => {
+                setPreview(index);
+            };
             listItem.textContent = fileName;
             fileList?.appendChild(listItem);
         });
+        const firstItem = fileList.querySelector("li");
+        if (firstItem) {
+            firstItem.classList.add("selected");
+        }
+        setPreview(0);
     }
     console.log(fileList);
 }
 
+function removeChildren(elem: HTMLElement) {
+    while (elem.firstChild) {
+        elem.removeChild(elem.firstChild);
+    }
+}
+
+async function setPreview(index: number) {
+    var previewData = await invoke("get_preview", {index});
+    if (previewData.hasOwnProperty("File")) {
+        console.log("file");
+        previewData = previewData['File'];
+        const preview = document.querySelector("#preview");
+        switch (previewData['kind']) {
+            case 'Image':
+                const base64 = previewData['content'];
+                if (preview) {
+                    preview.innerHTML = `<img class="previewContent" src="data:image/jpeg;base64,${base64}">`;
+                }
+                break;
+            default:
+                if (preview) {
+                    preview.innerHTML = `<p class="previewContent">${previewData['content']}</p>`;
+                }
+                break;
+        }
+    }
+    else {
+        // TODO: handle directories
+        console.log(previewData);
+    }
+}
+
 async function init() {
-    const dir = await invoke("get_current_path");
-    currentDir = dir;
-    const dirElem = document.querySelector("#currentDir"); 
+    const dirElem = document.querySelector("#currentDir");
     if (dirElem) {
+        const dir = await invoke("get_current_path");
         dirElem.textContent = dir;
+        updateFileList();
+        dirElem.onclick = async () => {
+            await invoke("go_to_parent");
+            const dir = await invoke("get_current_path");
+            dirElem.textContent = dir;
+            updateFileList();
+        }
     }
 }
 
@@ -29,5 +73,4 @@ async function init() {
 window.addEventListener("DOMContentLoaded", () => {
     fileList = document.querySelector("#fileList");
     init();
-    getFiles();
 });
