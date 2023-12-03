56a39fb478b36eea9861d9a3e617c4c8d05659a7
Heshan Padmasiri <hpheshan@gmail.com>
2023-12-03 09:44:38 +0530

    Implement keybord navigation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { invoke } from "@tauri-apps/api/tauri";
 
 let selectedIndex = 0;
+let maxIndex = 0;
 
 async function updateFileList() {
     const dirElem = document.querySelector("#currentDir");
@@ -13,6 +14,7 @@ async function updateFileList() {
     if (fileList) {
         removeChildren(fileList);
         const files = await invoke("get_files");
+        maxIndex = files.length - 1;
         files.forEach((fileName, index) => {
             const listItem = document.createElement("li");
             listItem.onclick = () => {
@@ -37,6 +39,8 @@ function removeChildren(elem: HTMLElement) {
 }
 
 async function onFileItemClick(index: number) {
+    selectedIndex = index;
+    // TODO: factor out preview from going to directory
     var previewData = await invoke("get_preview", {index});
     if (previewData.hasOwnProperty("File")) {
         previewData = previewData['File'];
@@ -60,16 +64,67 @@ async function onFileItemClick(index: number) {
         await invoke("go_to_directory", {index});
         await updateFileList();
     }
+    updateSelectedIndicator();
+}
+
+async function updatePreview() {
+    var previewData = await invoke("get_preview", {index: selectedIndex});
+    if (previewData.hasOwnProperty("File")) {
+        previewData = previewData['File'];
+        const preview = document.querySelector("#preview");
+        switch (previewData['kind']) {
+            case 'Image':
+                const base64 = previewData['content'];
+                if (preview) {
+                    preview.innerHTML = `<img class="previewContent" src="data:image/jpeg;base64,${base64}">`;
+                }
+                break;
+            default:
+                if (preview) {
+                    preview.innerHTML = `<p class="previewContent">${previewData['content']}</p>`;
+                }
+                break;
+        }
+    }
+
+}
+
+async function updateSelectedIndicator() {
     const fileList = document.querySelector("#fileList");
     const selected = fileList?.querySelector(".selected");
     if (selected) {
         selected.classList.remove("selected");
     }
-    const listItem = fileList?.querySelectorAll("li")[index];
+    const listItem = fileList?.querySelectorAll("li")[selectedIndex];
     if (listItem) {
         listItem.classList.add("selected");
     }
 }
+
+async function onKeyPress(event: KeyboardEvent) {
+    switch (event.key) {
+        case "j":
+            if (selectedIndex < maxIndex) {
+                selectedIndex++;
+                await updateSelectedIndicator();
+            }
+            break;
+        case "k":
+            if (selectedIndex > 0) {
+                selectedIndex--;
+                await updateSelectedIndicator();
+            }
+            break;
+        case "h":
+            await goToParent();
+            return;
+        case "l": // If it is file this will just show the preview so not a problem
+        case "Enter":
+            await onFileItemClick(selectedIndex);
+            return;
+    }
+    await updatePreview();
+};
 
 async function init() {
     const dirElem = document.querySelector("#currentDir");
@@ -81,6 +136,7 @@ async function init() {
             await goToParent();
         }
     }
+    document.addEventListener("keypress", onKeyPress);
 }
 
 async function goToParent() {
